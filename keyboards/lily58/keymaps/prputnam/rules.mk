d2f5262d4a8618f318604695a7b066fe16dc6b06
Patrick Putnam <patrick.putnam@zenplanner.com>
2021-05-03 23:11:07 -0600

    Set bootloader for Elite-C

--- keyboards/lily58/keymaps/prputnam/rules.mk
+++ keyboards/lily58/keymaps/prputnam/rules.mk
@@ -1,3 +1,5 @@
+BOOTLOADER = atmel-dfu  # Elite-C
+
 # Build Options
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
